323f78b37cf12b3b613bc9151877aaad98c4cf7c
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-06-22 14:45:34 +0600

    added code for CartBtton

--- src/components/CardButton/CardButton.js
+++ src/components/CardButton/CardButton.js
@@ -1,7 +1,13 @@
-export default function CartButton({ productId }) {
+import { useDispatch } from "react-redux";
 
-  
-    return (
-      <button className="CartButton">Add to card</button>
-    );
+export default function CartButton({ productId }) {
+  const dispatch = useDispatch();
+
+  function onAddToCart() {
+    dispatch({ type: 'cart/add', payload: productId });
   }
+
+  return (
+    <button className="CartButton" onClick={onAddToCart}>Add to card</button>
+  );
+}

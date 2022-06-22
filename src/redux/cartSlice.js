13169cfe6db9d1c9298224802aab4749c43c1a13
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-06-22 14:30:58 +0600

    added new folder

---
+++ src/redux/cartSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addToCart:(store,action) => {
+    }
+
+  }
+});
+
+export default cartSlice.reducer;

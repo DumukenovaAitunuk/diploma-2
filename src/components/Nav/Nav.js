3ac945036f55dcd036f9acace9a33cff3dca8a38
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-06-30 02:24:33 +0600

    Added new button

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
 import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem";
 
 function Nav() {
+  const isAuthenticated = useSelector(store => store.auth.idToken !== null);
+
   return (
     <ul className={classes.Nav}>
       <NavItem url="/">Home</NavItem>
       <NavItem url="/contacts">Contacts</NavItem>
-      <NavItem url="/payment">Payment</NavItem>
+      { isAuthenticated ? <NavItem url="/signout">Sign out</NavItem> : null }
+      { !isAuthenticated ? <NavItem url="/auth">Sign in</NavItem> : null }
     </ul>
   );
 }

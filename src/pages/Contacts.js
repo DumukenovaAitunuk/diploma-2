f5114cdd93b80f69dc6417696ce1dec6aa86c122
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-06-30 02:21:26 +0600

    solve error

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -4,11 +4,14 @@ import headerImage from "../assets/contacts.jpeg";
 function Contacts() {
   return (
     <>
-      <Header title="our contacts!" image={headerImage}>
+      <Header title="Our contacts!" image={headerImage}>
         Contributions are made by a large number of volunteers at their own
         discretion. Edits are neither the responsibility of the Wikimedia
         Foundation
       </Header>
+      <div>
+ 
+      </div>
     </>
   );
 }

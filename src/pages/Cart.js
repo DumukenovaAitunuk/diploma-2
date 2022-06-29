8908ed74ae50faff6091b76dc33c421878ceff85
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-06-30 01:35:47 +0600

    Added classes for tegs

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,22 +1,37 @@
 import Header from "../components/Header/Header";
 import image from "../assets/cart.jpeg";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../data/products";
 import { Link } from "react-router-dom";
+import { decrement, increment, remove } from "../redux/cartSlice";
 
 function Cart() {
   const items = useSelector(store => store.cart.items);
+  const dispatch = useDispatch();
   const products = getProducts();
 
-  const output = products
+  let total = 0;
+  let output = products
     .filter(product => items[product.productId])
     .map(product => {
-    return (
-      <div>
-        <Link to="">{product.title}</Link> {items[product.productId]} ${product.price * items[product.productId]}
-      </div>
-    );
-  });
+      total += product.price * items[product.productId];
+
+      return (
+        <div  className="Crement">
+          <Link to="">{product.title}</Link> {items[product.productId]} ${product.price * items[product.productId]}
+       
+          <button onClick={() => dispatch(decrement(product.productId))}>-</button>
+          <button onClick={() => dispatch(increment(product.productId))}>+</button>
+          <button onClick={() => dispatch(remove(product.productId))}>Delete</button>
+    
+        </div>
+      );
+    });
+
+  if (!output) {
+    output = "No items in the cart.";
+  }
+
 
   return (
     <>
@@ -25,8 +40,12 @@ function Cart() {
         image={image}>
         Please review items in your cart.
       </Header>
-      <div>
+      <div className="Total">
         {output}
+        <hr />
+        Total: ${total}
+
+        <Link to="/checkout">Checkout</Link>
       </div>
     </>
   );
